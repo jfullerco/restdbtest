49af47b1dd992d6774c658e915e621a2ff3ab69a
jfullerco <jonathan@jfuller.co>
2020-08-30 23:22:22 -0400

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import Axios from 'axios'
 import "./style.css";
 
+const url = 'https://tiems-d1ca.restdb.io/rest/clients'
+class dataBreakout extends React.Component { 
+render() {  
+  axios.get(url).then(data=>console.log(data))
+return(data)
+}
+}
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>{dataBreakout.dataItem}</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
